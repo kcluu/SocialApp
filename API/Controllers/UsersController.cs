7f72207d0f1085a07fcfdfe48665517984f01804
Katelyn Luu <kcluu16@gmail.com>
2022-11-30 11:21:28 -0500

    End of section 2

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,38 @@
+using Microsoft.AspNetCore.Mvc;
+using API.Data;
+using API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")] // GET /api/users <- how users will access this controller
+
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        // in order to inject anything in a class you must create a constructor:
+        // DataContext is a dependency; this allows us to have a session of our database available
+        public UsersController(DataContext context)
+        {
+            _context = context;
+            
+        }
+
+        [HttpGet] // this declares we're using an HTTP Get method to get the API/users
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers() // returns all of the users from the database
+        {
+            var users = await _context.Users.ToListAsync();
+
+            return users;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AppUser>> GetUser(int id) // returns a single user given their ID
+        {
+            var user = await _context.Users.FindAsync(id);
+            return user;
+        }
+    }
+}
